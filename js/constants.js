ad74e83f2df6b21a4c6ea39466b4df320aeae5ab
IlijazM <35637072+IlijazM@users.noreply.github.com>
2020-04-03 20:04:56 +0200

    Added a slash and dot

--- js/constants.js
+++ js/constants.js
@@ -62,7 +62,7 @@ C["h"] = "'h'"
 C["i"] = combinationIndex("undefined", 5)
 C["j"] = combinationIndex("[object Object]", 3)
 C["l"] = combinationIndex("false", 2)
-C["m"] = zero().constructor().toString().indexOf(11)
+C["m"] = zero().constructor().toString().indexAt(11)
 C["n"] = combinationIndex("undefined", 1)
 C["o"] = combinationIndex("[object Object]", 1)
 C["p"] = "'p'"
@@ -72,25 +72,25 @@ C["s"] = combinationIndex("false", 3)
 C["t"] = combinationIndex("true", 0)
 C["u"] = combinationIndex("true", 2)
 C["v"] = "'v'"
-C["y"] = newArray().constructor().toString().indexOf(13)
+C["y"] = newArray().constructor().toString().indexAt(13)
 
 // brackets
 
 C["["] = combinationIndex("[object Object]", 0)
 C["]"] = combinationIndex("[object Object]", 14)
-C["("] = newArray().constructor().toString().indexOf(14)
-C[")"] = newArray().constructor().toString().indexOf(15)
-C["{"] = newArray().constructor().toString().indexOf(17)
-C["}"] = newArray().constructor().toString().indexOf(33)
-C["<"] = emptyString().indexOf("italics").add("()").indexOf(0)
-C[">"] = emptyString().indexOf("italics").add("()").indexOf(2)
+C["("] = newArray().constructor().toString().indexAt(14)
+C[")"] = newArray().constructor().toString().indexAt(15)
+C["{"] = newArray().constructor().toString().indexAt(17)
+C["}"] = newArray().constructor().toString().indexAt(33)
+C["<"] = emptyString().indexAt("italics").add("()").indexAt(0)
+C[">"] = emptyString().indexAt("italics").add("()").indexAt(2)
 
 // upper case
 
-C["A"] = newArray().constructor().toString().indexOf(9)
+C["A"] = newArray().constructor().toString().indexAt(9)
 C["N"] = combinationIndex("NaN", 0)
-C["O"] = newObject().constructor().toString().indexOf(9)
-C["S"] = emptyString().constructor().toString().indexOf(9)
+C["O"] = newObject().constructor().toString().indexAt(9)
+C["S"] = emptyString().constructor().toString().indexAt(9)
 
 C["k"] = btoaFunction(19, 2)
 C["w"] = btoaFunction(3, 1)
@@ -106,6 +106,12 @@ C["Q"] = btoaFunction(1, 1)
 C["T"] = btoaFunction(10, 1)
 C["U"] = btoaFunction(15, 2)
 C["Y"] = btoaFunction(16, 2)
+
+C["="] = btoaFunction(0, 3)
+C["/"] = emptyString().indexAt("italics").add("()").indexAt(4)
+
+// C["'"] = newFunction("'return atob()'")
+C["."] = newFunction(generateString("return 1/2")).toString().indexAt(1)
 
 function getNumberValue(index) {
     let out = ""
@@ -138,10 +144,10 @@ function toValue(value) {
         inCurlyBrackets: () => { return toValue(`{${value}}`) },
         inSquareBrackets: () => { return toValue(`[${value}]`) },
         toString: () => { return toValue(value + TOSTRING) },
-        indexOf: (i) => { return toValue(`(${value})[${getNumberValue(i)}]`) },
+        indexAt: (i) => { return toValue(`(${value})[${getNumberValue(i)}]`) },
         constructor: (i) => { return toValue(`(${value})${CONSTRUCTOR}`) },
         toFunction: () => {
-            return toValue(toValue("[]").indexOf("sort").constructor().value + `(${value})()`)
+            return toValue(toValue("[]").indexAt("sort").constructor().value + `(${value})()`)
         },
         add: (other) => { return toValue(value + other) },
         eval: () => {
@@ -155,15 +161,22 @@ function getNumbersRaw(n) { return toValue(getNumberValue(n)) }
 function getNumber(n) { return getNumberRaw(n).toString() }
 function getNumbers(n) { return getNumbersRaw(n); }
 function combination(c) { return toValue(COMBINATIONS[c]).toString() }
-function combinationIndex(c, i) { return combination(c).toString().indexOf(i) }
+function combinationIndex(c, i) { return combination(c).toString().indexAt(i) }
 
 function newArray() { return toValue("[]") }
 function newObject() { return toValue("{}") }
-function zero() { return toValue("(+[])") }
-function emptyString() { return toValue("([]+[])") }
+function zero() { return toValue("+[]") }
+function emptyString() { return toValue("[]+[]") }
 function newFunction(name) { return toValue(name).toFunction() }
 
-function btoaFunction(b, i) { return toValue(generateString(`return btoa(${b})`)).toFunction().toString().indexOf(i) }
+function btoaFunction(b, i) { return toValue(generateString(`return btoa(${b})`)).toFunction().toString().indexAt(i) }
+
+function forceChar(c) {
+    c = c.charCodeAt(0)
+    console.log(c)
+
+    return c
+}
 
 function generateString(input) {
     let out = ""
@@ -173,7 +186,7 @@ function generateString(input) {
 
         let c = C[input[i]]
 
-        if (c == undefined) c = `"${input[i]}"`
+        if (c == undefined) out += forceChar(input[i])
         else out += c.value;
     }
 
