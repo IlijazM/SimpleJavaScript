575fd7490347772612099ad53efe40277c2c75c0
programminghoch10 <16062290+programminghoch10@users.noreply.github.com>
2020-04-04 14:16:54 +0200

    UI improvements

--- js/app.js
+++ js/app.js
@@ -2,11 +2,33 @@ getel = (name) => { return document.getElementById(name) }
 getvalue = (name) => { return getel(name).value }
 
 function toText() {
-    let text = getvalue("text")
-    getel("js").value = generateString(text)
+    let text = getvalue("input")
+    getel("outputjs").value = generateString(text)
 }
 
 function toJS() {
-    let text = getvalue("text")
-    getel("js").value = generateFunction(text)
+    let text = getvalue("input")
+    getel("outputjs").value = generateFunction(text)
 }
+
+function obfuscate() {
+    if (getel("textselector").checked) {
+        toText();
+    } else if (getel("jsselector").checked) {
+        toJS();
+    } else {
+        getel("outputjs").value = "Please select input type!"
+    }
+    getel("outputdiv").style.display = "";
+}
+
+function updateplaceholder() {
+    if (getel("textselector").checked) {
+        getel("input").placeholder = "Enter some Text...";
+    } else if (getel("jsselector").checked) {
+        getel("input").placeholder = "Enter some JavaScript...";
+    } else {
+        getel("input").placeholder = "Please select input type!"
+    }
+}
+updateplaceholder();
